f3679d11414ba177da789a7d38a5146f39944d83
Dineshynr <88737079+Dineshynr@users.noreply.github.com>
2021-08-23 02:16:08 +0530

    Add files via upload

---
+++ hh.php
@@ -0,0 +1,7 @@
+<?php
+
+    $NAME = $_POST['fname'];
+    $mail = $_POST['mail'];
+    $password = $_POST['psw'];
+    echo"Congrats Your dta Is Stolen by Dinesh <br> ". $NAME ." ". $mail." ". $password;
+?>
