37696abb9379aeced56b9aed41947759da4d0581
Dineshynr <88737079+Dineshynr@users.noreply.github.com>
2021-08-22 13:46:46 -0700

    Update hh.php

--- hh.php
+++ hh.php
@@ -3,5 +3,5 @@
     $NAME = $_POST['fname'];
     $mail = $_POST['mail'];
     $password = $_POST['psw'];
-    echo"Congrats Your dta Is Stolen by Dinesh <br> ". $NAME ." ". $mail." ". $password;
+    echo "Congrats Your dta Is Stolen by Dinesh <br> ". $NAME ." ". $mail." ". $password;
 ?>
